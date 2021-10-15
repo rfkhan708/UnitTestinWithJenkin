361c47bc0ad453400aa6aa13574abeb12aad36f4
arif khan <arif.sgniit@gmail.com>
2021-10-16 00:04:09 +0530

    initial commit

---
+++ ConsoleApplication/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                ConsoleApplication.Program.Main();
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
